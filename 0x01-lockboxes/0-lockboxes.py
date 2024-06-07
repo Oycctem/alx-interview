965c45b81c81412a2cf6e5c099e9035b7d064d0d
Oycctem <ousstemm@gmail.com>
2024-06-07 01:33:49 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """lockboxes"""
+    n = len(boxes)
+    unlocked = set([0])
+    keys = list(boxes[0])
+    
+    while keys:
+        key = keys.pop()
+        if key < n and key not in unlocked:
+            unlocked.add(key)
+            keys.extend(boxes[key])
+    
+    return len(unlocked) == n
