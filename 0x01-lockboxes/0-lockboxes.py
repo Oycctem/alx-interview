1a48fd9f452e049c1fd04e8e9e4fd258a80c2d23
Oycctem <ousstemm@gmail.com>
2024-06-07 01:36:14 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,11 +7,9 @@ def canUnlockAll(boxes):
     n = len(boxes)
     unlocked = set([0])
     keys = list(boxes[0])
-    
     while keys:
         key = keys.pop()
         if key < n and key not in unlocked:
             unlocked.add(key)
             keys.extend(boxes[key])
-    
     return len(unlocked) == n
