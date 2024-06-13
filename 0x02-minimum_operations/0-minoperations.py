df689901357807ab7163bce2d8c0f6f36cb31adf
Oycctem <ousstemm@gmail.com>
2024-06-13 23:51:57 +0100

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+""""""
+
 def minOperations(n):
+    """
+    Returns the fewest number of operations to get n H characters.
+    """
     if n < 2:
         return 0
 
     operations = 0
-    factor = 2
+    min_operations = 2
 
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        while n % min_operations == 0:
+            operations += min_operations
+            n //= min_operations
+        min_operations += 1
 
     return operations
