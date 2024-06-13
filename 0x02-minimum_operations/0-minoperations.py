8b8df02cf97da0bb1768054216df50d4706b4400
Oycctem <ousstemm@gmail.com>
2024-06-13 23:58:14 +0100

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""""""
+""" Minimum Operations to get n H characters """
 
-
-def minOperations(n):
-    """"""
+def minOperations(n: int) -> int:
+    """ Returns the fewest number of operations needed to result in exactly n H characters """
+    if n < 2:
+        return 0
+    
     operations = 0
-    min_operations = 2
+    factor = 2
+    
     while n > 1:
-        while n % min_operations == 0:
-            operations += min_operations
-            n /= min_operations
-        min_operations += 1
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
     return operations
